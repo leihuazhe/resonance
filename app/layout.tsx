b443003a9568920fc6cfcf80be85ac9f34252be6
leihuazhe <leihuazhe@sensorsdata.cn>
2024-05-28 12:55:06 +0800

    feat(149-2): Reserved, time:  2024-05-28 12:55:06

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import CustomSearchProvider from '@/components/CustomSearchProvider'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -80,10 +81,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
-              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+              {/* <SearchProvider searchConfig={siteMetadata.search as SearchConfig}> */}
+              <CustomSearchProvider>
                 <Header />
                 <main className="mb-auto">{children}</main>
-              </SearchProvider>
+              </CustomSearchProvider>
               <Footer />
             </div>
           </SectionContainer>
